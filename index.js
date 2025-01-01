0cdbbb8f339f234fcb90333b9d70fb952c4781e6
yahya momin <yahyamomin06@gmail.com>
2025-01-01 15:17:14 +0530

    add route

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const express = require('express');
-const { M3u8Proxy } = require('./logic/proxy');
-const app = express();
+const express = require('express')
+const { M3u8Proxy } = require('./logic/proxy')
+const app = express()
 
 // Middleware to parse JSON query params
-app.use(express.json());
-
+app.use(express.json())
 
 // Define routes
 app.options('/proxy', (req, res) => {
-    // Handle OPTIONS request for CORS preflight
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    return res.status(204).end();  // Respond with no content
-});
+  // Handle OPTIONS request for CORS preflight
+  res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  return res.status(204).end() // Respond with no content
+})
 
+app.get('/', (req, res) => {
+  res.send('m3u8 proxy')
+})
 app.get('/proxy', (req, res) => {
-    M3u8Proxy(req, res);
-});
+  M3u8Proxy(req, res)
+})
 
 // Handle 404
 app.use((req, res) => {
-    res.status(404).send('Not Found');
-});
+  res.status(404).send('Not Found')
+})
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
